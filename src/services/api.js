12570bf763e9ec8b66ef489acbdd1673e2ad7d64
Exlley Santos <exlleyclemente@gmail.com>
2021-07-07 19:15:46 -0300

    add env

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 import { getToken } from './auth';
+require('dotenv/config'); 
 
-const api = axios.create({ baseURL: 'http://localhost:3333' });
+const api = axios.create({ baseURL: process.env.REACT_API_URL});
 
 api.interceptors.request.use((config) => {
 	const token = getToken();
