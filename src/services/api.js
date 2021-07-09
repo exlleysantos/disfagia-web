252c029775706f7669d2a7c74f8271a32b213c1d
Exlley Santos <exlleyclemente@gmail.com>
2021-07-09 19:15:42 -0300

    Config API

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import { getToken } from './auth';
 
-const api = axios.create({ baseURL: 'https://api-disfagia.herokuapp.com' });
+const api = axios.create({ baseURL: process.env.REACT_APP_API_URL });
 
 api.interceptors.request.use((config) => {
 	const token = getToken();
